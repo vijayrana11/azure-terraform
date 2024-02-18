d210a200178e0416758e4212f4c0fb0362080901
Ubuntu <terraform@terraform-agent.afehveylwasufnbgbojsvoqrrg.rx.internal.cloudapp.net>
2024-02-18 16:36:46 +0000

    adding required filed

--- azure-resources/virtualmachine.tf
+++ azure-resources/virtualmachine.tf
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1ae814fcce54f2bad07c1cf7d2a363c8768ffe6186394efc9031cc6f140b2c3b
-size 1198
+oid sha256:42f5b7111a222c9ae9d0978ae1d26f4dbef0072ce766fac47d70cf7ec581818a
+size 1231
