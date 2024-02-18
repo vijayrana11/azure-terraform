74775adb17af8a46713af36c80ade93853d39d96
vijay rana <vijay.rana@adani.com>
2024-02-18 16:07:37 +0530

    adding changes

--- azure-resources/rg-variable.tf
+++ azure-resources/rg-variable.tf
@@ -1,3 +1,19 @@
+<<<<<<< HEAD
 version https://git-lfs.github.com/spec/v1
 oid sha256:002a4b897a61fad0fd413f959b05b4a539672c90d1482ff24c1c7d89900e83d8
 size 242
+=======
+variable "rgname" {
+    description = "this is the rg name "
+  
+}
+variable "location" {
+    description = "this is the location where resources wll create"
+     
+  
+}
+variable "managedby" {
+    description = "tagged with whome this resource was created"
+  
+}
+>>>>>>> 77376a7 (adding changes)
