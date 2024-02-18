9e1c83c5442a98aef28f222775bed20284f8c1a2
Ubuntu <terraform@terraform-agent.afehveylwasufnbgbojsvoqrrg.rx.internal.cloudapp.net>
2024-02-18 05:17:59 +0000

    adding changes

--- azure-resources/provider.tf
+++ azure-resources/provider.tf
@@ -1,18 +1,3 @@
-# Terraform Block
-terraform {
-  required_version = ">= 1.0.0"
-  required_providers {
-    azurerm = {
-        source = "hashicorp/azurerm"
-        version = ">= 2.0"
-    }
-  }
-}
-# provider block
-
-provider "azurerm" {
-    features {
-      
-    }
-  
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:f263ba72af6c3f147b66398fa35dea3c82d38badd3347e6b4bba3dc9aaf6cd70
+size 248
--- azure-resources/resource-group.tf
+++ azure-resources/resource-group.tf
@@ -1,6 +1,3 @@
-resource "azurerm_resource_group" "myrg" {
-    name = "terraform-automated-rg"
-    location = "Central India"
-    managed_by = "teform-automation"
-  
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:c07ae765c05fe769ec2d415ee5d5506430371cd593c88525dadc57663ebef33e
+size 152
